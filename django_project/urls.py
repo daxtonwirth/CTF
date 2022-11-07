34a618023749887f9f43020fda1a39b65a8523a6
Dax <daxtonwirth@gmail.com>
2022-11-07 11:37:43 -0800

    working website

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("django.contrib.auth.urls")),
-    path('', TemplateView.as_view(template_name='index.html'), name='home')
+    path("", TemplateView.as_view(template_name='index.html'), name='home'),
+    path("register", views.register_request, name="register")
 ]
