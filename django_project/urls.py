9d286ea9586f714400b03a10fe94621dbbdc39ca
Dax <daxtonwirth@gmail.com>
2022-11-08 12:21:58 -0800

    fixed registration bug

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from . import views
+from register import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("django.contrib.auth.urls")),
     path("", TemplateView.as_view(template_name='index.html'), name='home'),
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("robots.txt", TemplateView.as_view(template_name='robots.txt'), name='robots'),
 ]
--- register/forms.py
+++ register/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class NewUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
 	email = forms.EmailField(required=True)
 
 	class Meta:
@@ -11,7 +11,7 @@ class NewUserForm(UserCreationForm):
 		fields = ("username", "email", "password1", "password2")
 
 	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
+		user = super(UserCreationForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
 		if commit:
 			user.save()
